4390cb12bf320bd1fe862ea8d6bab34d952aaf9f
Lita Beach <litabeach123@gmail.com>
2021-05-10 14:45:18 -0500

    login and logout routes added, JWT tokens included

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post("/", async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true
         })
-        .send();
+            .send();
 
 
     } catch (err) {
@@ -74,5 +74,65 @@ router.post("/", async (req, res) => {
         res.status(500).send()
     }
 });
+
+//login
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //validate 
+        if (!email || !password)
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter all required fields."
+                });
+
+        const existingUser = await User.findOne({ email })
+        if (!existingUser)
+            return res
+                .status(401)
+                .json({
+                    errorMessage: "Wrong email or password."
+                });
+
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash)
+        if (!passwordCorrect)
+            return res
+                .status(401)
+                .json({
+                    errorMessage: "Wrong email or password."
+                });
+        
+        //sign the token
+        const token = jwt.sign(
+            {
+                user: existingUser._id
+            },
+            process.env.JWT_SECRET
+        );
+
+        //send the token in an HTTP only cookie
+        res.cookie("token", token, {
+            httpOnly: true
+        })
+            .send();
+
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send()
+    }
+});
+
+//logout
+router.get("/logout", (req, res) => {
+    res.cookie("token", "",{
+    httpOnly: true,
+    expires: new Date(0)
+    })
+    .send();
+});
+
 
 module.exports = router;
