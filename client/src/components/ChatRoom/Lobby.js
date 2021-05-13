272444e1b95d41a6e060f69e4f9e3a4795df413a
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-13 13:32:42 -0500

    added dropdwon menu to lobby

--- client/src/components/ChatRoom/Lobby.js
+++ client/src/components/ChatRoom/Lobby.js
@@ -23,13 +23,20 @@ const Lobby = ({
 
       <div>
         <label htmlFor="room">Room name:</label>
-        <input
+        <select id="room" value={roomName} onChange={handleRoomNameChange} required>
+            <option selected> Select</option>
+            <option value="Room One">Room One</option>
+            <option value="Room Two">Room Two</option>
+            <option value="Room Three">Room Three</option>
+            <option value="Room Four">Room Four</option>
+          </select>
+        {/* <input
           type="text"
           id="room"
           value={roomName}
           onChange={handleRoomNameChange}
           required
-        />
+        /> */}
       </div>
       <button type="submit">Submit</button>
     </form>
