7662bdac5666a75be7bae1e777b40f28be58d474
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-15 20:19:50 -0500

    Code Cleanup

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  // process.env.MONGODB_URI ||
+   process.env.MONGODB_URI ||
    "mongodb://localhost/sleep", 
   {
     useCreateIndex: true,
