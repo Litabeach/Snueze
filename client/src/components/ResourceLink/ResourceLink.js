ce149ee0ffb99dad5ae17a690fb33570d5e04bfe
Lita Beach <litabeach123@gmail.com>
2021-05-17 14:12:56 -0500

    simple resources page created

---
+++ client/src/components/ResourceLink/ResourceLink.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function ResourceLink(props) {
+    return (
+        <div className="resource-card">
+            <h5><a href={props.link} className="resource-links"><img src={props.image1} className="img-fluid resource-img"
+                alt={props.alt1} />
+            {props.title} </a> </h5>
+            <p>{props.description} </p>
+            {/* <p><a href={props.link} className="links">Visit {props.title} <br />
+            </a></p> */}
+        </div>
+
+
+    );
+}
+
+export default ResourceLink;
