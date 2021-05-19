003dfdedefe7ead38aaf6ac39bca3e08a5cc4af4
Lita Beach <litabeach123@gmail.com>
2021-05-19 13:38:31 -0500

    register page has usestate to handle inputs

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,17 +1,43 @@
 import React from 'react'
 
 function Register() {
+
+    const[userName, setUserName] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[passwordVerify, setPasswordVerify] = useState("");
+
     return (
         <div>
             <h1> Register a new account</h1>
             <form>
-                <input type="username" placeholder="Username"/>
+                <input 
+                // type="username" 
+                placeholder="Username"
+                onChange={(e) => setUserName(e.target.value)}
+                value={userName}
+                />
                 <br />
-                <input type="email" placeholder="Email"/>
+                <input 
+                type="email" 
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                />
                 <br />
-                <input type="password" placeholder="Password"/>
+                <input 
+                type="password" 
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                />
                 <br />
-                <input type="password" placeholder="Verify Your Password"/>
+                <input 
+                type="password" 
+                placeholder="Verify Your Password"
+                onChange={(e) => setPasswordVerify(e.target.value)}
+                value={passwordVerify}
+                />
                 <br />
                 <button type="submit">Register</button>
             </form>
