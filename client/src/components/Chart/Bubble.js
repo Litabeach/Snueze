2652d59a60fedc8e1573c221928641ce468e3cdf
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-19 12:02:33 -0500

    Logic written to return notes by date

--- client/src/components/Chart/Bubble.js
+++ client/src/components/Chart/Bubble.js
@@ -53,11 +53,11 @@ function BubbleChart() {
       };
 
       const yLabels = {
-          0 : '00:00', 1 : '01:00', 2 : '02:00', 3 : '03:00', 4 : '04:00',
+          1 : '01:00', 2 : '02:00', 3 : '03:00', 4 : '04:00',
           5 : '05:00', 6 : '06:00', 7 : '07:00', 8 : '08:00',
           9 : '09:00', 10 : '10:00',  11 : '11:00', 12 : '12:00', 13 : '13:00', 14 : '14:00',
           15 : '15:00', 16 : '16:00', 17 : '17:00', 18 : '18:00',
-          19 : '19:00', 20 : '20:00', 21: '21:00',  22 : '22:00', 23 : '23:00', 24: "24:00"
+          19 : '19:00', 20 : '20:00', 21: '21:00',  22 : '22:00', 23 : '23:00', 24: "00:00"
         }
         
     
@@ -78,9 +78,9 @@ function BubbleChart() {
                 text: "Bed Time in Military Time",
               },
               ticks: {
-                                  callback: function(value, index, values ) {
+                  callback: function(value, index, values ) {
                                       // for a value (tick) equals to 8
-                                      return yLabels[value];
+                    return yLabels[value];
                                       // 'junior-dev' will be returned instead and displayed on your chart
                                   }
                               }
---
+++ client/src/components/Notes/index.js
@@ -0,0 +1,68 @@
+import { CollectionsBookmarkRounded } from "@material-ui/icons";
+import React, { useState } from "react";
+// import surveyAPI from "../../utils/surveyAPI";
+import surveyAPI from "../../utils/surveyAPI";
+
+//change to bubble
+function Notes() {
+
+   
+    const [searchDate, setSearchDate] = useState();
+    const [note, setNote] = useState();
+
+    function handleInputChange(event) {
+        const { value } = event.target;
+        console.log("Handle change" + value);
+        setSearchDate(value);
+        // console.log("Search Date" + searchDate)
+    };
+
+   
+    function handleNoteSearch(date) {
+        console.log(searchDate)
+
+        let notes = [];
+        
+     surveyAPI.getSurveys()
+            .then(res => {
+                console.log(res.data)
+
+                let data = res.data
+
+                data.forEach(dateData => {
+                    let splitDate = dateData.date.split("T")
+                    let justDate = splitDate[0]
+                    
+                    if (justDate == date){
+                        notes.push(dateData.notes);
+                    } 
+                })
+
+                console.log(notes)
+                setNote(notes);
+               
+               
+              
+            })
+            .catch(err => console.log(err));
+    }
+
+
+
+
+
+
+    return (
+        <div>
+            <div className="form-group">
+            <input type="date" name="date" onChange={handleInputChange}></input>
+               
+            </div>
+            <button className="submitBtn" onClick={() => handleNoteSearch(searchDate)}>Submit</button>
+            <h1>{note}</h1>
+        </div>
+    );
+}
+
+
+export default Notes
