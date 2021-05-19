12cff76c59680206cd95153cc5011c4b11780024
Lita Beach <litabeach123@gmail.com>
2021-05-19 13:09:07 -0500

    working auth midlewaregit add .

--- utils/auth.js
+++ utils/auth.js
@@ -1,17 +1,26 @@
-const jwt = require("jsonwebtoken");
-
-module.exports = (req, res, next) => {
-    try { const token = req.headers.authorization.split(" ")[1];
-   const decodedToken = jwt.verify(
-     token,
-     process.env.JWT_SECRET
-  ); req.userData = {
-           email: decodedToken.email,
-     };    next();
-    } catch (error) {
-      res.status(401).json({ message: "Auth failed!" });
-    }
-  };
+const jwt = require("jsonwebtoken")
+const dotenv = require("dotenv");
+dotenv.config();
 
 
-  //client side application of JWT(scroll down): https://itnext.io/authentication-in-mern-stack-using-jwt-25c966027f77
+function auth(req, res, next) {
+  try {
+      const token = req.cookies.token;
+
+      if(!token) return  res.status(401).json({ errorMessage: "Unauthorized" });
+
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = verified.user;
+
+      next();
+
+  } catch (err) {
+      console.error(err);
+      res.status(401).json({ errorMessage: "Unauthorized" });
+  }
+}
+
+
+
+module.exports = auth
