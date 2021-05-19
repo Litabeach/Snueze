89fb262c56fc958943568eaa21e020f292a92179
SageTengu <kyle.kleven@outlook.com>
2021-05-18 20:32:08 -0500

    Sleep survey is wired up, also made a few styling tweaks: site background image addedand changed tools button styling to point to a class

--- client/src/pages/MyBed/MyBed.js
+++ client/src/pages/MyBed/MyBed.js
@@ -12,11 +12,11 @@ function MyBed() {
       My Bed
       </Jumbotron>
 
-      <Link to="/Journal">Journal<a href="https:// placeholder.com"><img src="https:/via.placeholder.com/150"></img></a>
+      <Link to="/Survey">Sleep<a href="/survey"><img src="https:/via.placeholder.com/150"></img></a>
       </Link>
 
-      <Link to="/Resources">Resources<a href="https:// placeholder.com"><img src="https:/via.placeholder.com/150"></img></a>
-      </Link>
+      <Link to="/Journal">Journal<a href="https:// placeholder.com"><img src="https:/via.placeholder.com/150"></img></a>
+      </Link>   
 
       <Link to="/Stats">Stats<a href="https:// placeholder.com"><img src="https:/via.placeholder.com/150"></img></a>
       </Link>
