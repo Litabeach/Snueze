555600567b4fff942749d91da55d36e3569f52f5
Lita Beach <litabeach123@gmail.com>
2021-05-20 12:09:40 -0500

    conditional rendering of navbar based on loggedIn status

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Nav } from 'react-bootstrap';
 import SpeechToTextNav from "../SpeechToTextNav/SpeechToTextNav";
 import "./style.css"
+import AuthContext from "../../context/AuthContext"
 
 function Header() {
+
+  const { loggedIn } = useContext(AuthContext);
+  console.log(loggedIn)
+
    return (
     <div className="nav">
     <Nav variant="pills" activeKey="1" >
     <a className="navbar-brand" href="/">Snüze</a>
-    <Nav.Item>
-    <Nav.Link href="/mybed" title="My Bed">
-      My Bed
-    </Nav.Link>
-  </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/resources" title="Resources">
-          Resources
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="Community" title="Community">
-          Community
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/logout" title="Logout">
-          Logout
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <SpeechToTextNav />
-      </Nav.Item>
+    {loggedIn && ( 
+          <>
+          <Nav.Item>
+          <Nav.Link href="/mybed" title="My Bed">
+            My Bed
+          </Nav.Link>
+        </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/resources" title="Resources">
+                Resources
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="Community" title="Community">
+                Community
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/auth/logout" title="Logout">
+                Logout
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+            <SpeechToTextNav />
+            </Nav.Item>
+            </>
+    )}
+        {!loggedIn && 
+        <>
+           <Nav.Item>
+              <Nav.Link href="/" title="Login">
+                Login
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link href="/register" title="Register">
+                Register
+              </Nav.Link>
+            </Nav.Item>
+        </>
+        }
+  
     </Nav>
     </div>
   )
