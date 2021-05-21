ec92fec4ab0ad8feffcdc32851bf69f0c0233999
SageTengu <kyle.kleven@outlook.com>
2021-05-20 21:20:01 -0500

    Updated register form component

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -36,16 +36,15 @@ function Login() {
   return (
     <div className="wrapper">
         <Form onSubmit={login}>
+        <h4>Log into Snüze</h4>
     <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} />
     </Form.Group>
     <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
     </Form.Group>
     <Button variant="primary" type="submit">
-        Submit
+        Login
     </Button>
     <Form.Text className="text-muted">
         Don't have a login? Click <a href="/register">here</a> to register.
