309800c875102547c038618e7f20b4516eb496f2
Lita Beach <litabeach123@gmail.com>
2021-05-21 12:19:10 -0500

    main

--- routes/api/survey.js
+++ routes/api/survey.js
@@ -4,12 +4,27 @@ const auth = require("../../utils/auth")
 
 // Matches with "/api/survey"
 router.route("/")
-  .get(auth, surveyController.findAll)
-  .post(auth, surveyController.create);
+  .get(surveyController.findAll)
+  .post(surveyController.create);
 
 // Matches with "/api/survey/:id"
 router
-  .route("/:id")
-  .get(auth, surveyController.findById);
+  .route("/user")
+  .get(surveyController.findById);
+
+router.get("/surveys", (req, res) => {
+  const authHeader = req.headers.cookie;
+  if (authHeader) {
+      const token = authHeader.split('=')[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, { surveys }) => {
+          if (err) {
+              return res.sendStatus(403);
+          }
+          res.json(surveys);
+      });
+  } else {
+      res.sendStatus(401);
+  }
+})
 
 module.exports = router;
