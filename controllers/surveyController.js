9f4b3c73be2865c50a916714b006fa2aa07c3000
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-22 13:26:06 -0500

    Chart now loading new entries

--- controllers/surveyController.js
+++ controllers/surveyController.js
@@ -1,48 +1,36 @@
 const db = require("../models");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const { isValidObjectId } = require("mongoose");
 
 // Defining methods for the surveyController
 module.exports = {
   findAll: function(req, res) {
     db.User
       .find({
-
       })
-      // .sort({ date: -1 })
+      .sort({ date: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  
   findById: function(req, res) {
-    const authHeader = req.headers.cookie;
-    if (authHeader) {
-        const token = authHeader.split('=')[1];
-        jwt.verify(token, process.env.JWT_SECRET, (err, { surveys }) => {
-            if (err) {
-                return res.sendStatus(403);
-            }
-            res.json(surveys);
-        });
+    if (req.cookies.userId) {
+        db.User.find({"_id": req.cookies.userId}).then(data => db.Survey.find({"_id": {$in: data[0].surveys}})
+        .then(data => res.json(data)))
     } else {
         res.sendStatus(401);
     }
   },
   create: function(req, res) {
-      const authHeader = req.headers.cookie;
-      if (authHeader) {
-          const token = authHeader.split('=')[1];
-          jwt.verify(token, process.env.JWT_SECRET, (err, {user}) => {
-              if (err) {
-                  return res.sendStatus(403);
-              }
-              db.Survey
-              .create(req.body)
-              .then(({_id}) => db.User.findByIdAndUpdate(user, {$push: {surveys: _id}}))
-              .then(userDoc => res.json(userDoc))
-              .catch(err => {console.log(err); res.status(422).json(err)});
-          });
-      } else {
-          res.sendStatus(401);
-      }
-  }
+
+    if (req.cookies.userId) {
+        db.Survey.create(req.body)
+        .then(({_id}) => db.User.findByIdAndUpdate(req.cookies.userId, {$push: {surveys: _id}}))
+        .then(userDoc => res.json(userDoc))
+        .catch(err => {console.log(err); res.status(422).json(err)});
+
+    } else {
+        res.sendStatus(401);
+    }
+  },
+
 };
