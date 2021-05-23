b7a7853a1de6f93bfe3af210914241e551c02416
Omaur B <omaurbliss@gmail.com>
2021-05-23 18:33:19 -0500

    New photos for mybed page

--- client/src/pages/MyBed/MyBed.js
+++ client/src/pages/MyBed/MyBed.js
@@ -10,7 +10,7 @@ function MyBed() {
         <CardGroup>
           <Card className="card">
             <Link to="/survey">
-              <Card.Img as={Image} className="cardImg" variant="top" src="img/sleep.jpeg" alt="Sleep Image" />
+              <Card.Img as={Image} className="cardImg cimg" variant="top" src="img/Snuze_photo_button_3_(1).png" alt="Sleep Image" />
               <Card.ImgOverlay>
                 <Card.Title>Record your sleep</Card.Title>
               </Card.ImgOverlay>
@@ -18,7 +18,7 @@ function MyBed() {
           </Card>
           <Card className="card">
             <Link to="/journal">
-              <Card.Img as={Image} className="cardImg" variant="top" src="img/dream.png" alt="Dream Image" />
+              <Card.Img as= {Image} className="cardImg" variant="top" src="img/Snuze_photo_button1.png" alt="on phone in bed image" />
               <Card.ImgOverlay>
                 <Card.Title>Log your dreams</Card.Title>
               </Card.ImgOverlay>
@@ -28,7 +28,7 @@ function MyBed() {
           <CardGroup>
           <Card className="card">
             <Link to="/stats">
-              <Card.Img as={Image} className="cardImg" variant="top" src="img/analyze.jpg" alt="Chart Image" />
+              <Card.Img as={Image} className="cardImg" variant="top" src="img/Snuze_photo_button_4(1).png" alt="Chart Image" />
               <Card.ImgOverlay>
                 <Card.Title>Analyze your sleep habits</Card.Title>
               </Card.ImgOverlay>
@@ -36,7 +36,7 @@ function MyBed() {
           </Card>
           <Card className="card">
             <Link to="/tools">
-              <Card.Img as={Image} className="cardImg" variant="top" src="img/reflect.jpg" alt="Reflect Image" />
+              <Card.Img as={Image} className="cardImg" variant="top" src="img/Snuzephotobutton2.png" alt="Reflect Image" />
               <Card.ImgOverlay>
                 <Card.Title>Reflect and relax</Card.Title>
               </Card.ImgOverlay>
