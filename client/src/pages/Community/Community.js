cb58c68ceb4b7b621cb14e0fe446e13cc09c49bf
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-24 10:30:37 -0500

    Chat Room copy added

--- client/src/pages/Community/Community.js
+++ client/src/pages/Community/Community.js
@@ -6,7 +6,31 @@ const Community = () => {
    
      
       <main>
+        <h1>Community Chat Rooms</h1> 
+        <h4>Sometimes all we're really looking for is a person to connect with over our problems or someone to go to for advice. Utilize our collection of chat rooms to talk to real people who are going through the same thing as you.</h4>
         <ChatRoom />
+        <h3>Chat Rooms</h3>
+          <h4>Dream Team</h4>
+            <h5>Dying to know why you keep dreaming about bees? Did you have a crazy dream and just need to share it? Head to this room to share all your stories from Dreamland.</h5>
+          <h4>Insomniacs</h4>
+            <h5>A general chat room for those who just can't get to sleep.</h5>
+          <h4>Pillow Talk</h4>
+            <h5>Is your pillow not working for you? Do you have a pillow you LOVE that has made all the difference in your sleep? Share it here!</h5>
+          <h4>Pregnancy and Sleep</h4>
+            <h5>Preganancy affects your whole life. Including your sleep cycle. This room is for mamas who want a good night's sleep.</h5>
+          <h4>Restless Leg Syndrome</h4>
+            <h5>Enter this room to connect with others suffering from Restless Leg Syndrome.</h5>
+          <h4>Sleep Aids</h4>
+            <h5>Melatonin? Unisom? Herbal teas? Enter this room to chat about which sleep aids work for you.</h5>
+          <h4>Sleep Apnea</h4>
+            <h5>Suffering from sleep apnea? Enter here to chat about equipment, struggles, and successes.</h5>
+          <h4>Sleep for Kids</h4>
+            <h5>Enter this room to chat about the challenges of our little ones' sleep schedules.</h5>
+          <h4>Sleep Walking </h4>
+            <h5>A chat room for those suffering from sleepwalking and its possible remedies.</h5>
+          <h4>Too Much Sleep </h4>
+            <h5>Are you sleeping too long at night? Enter here to chat with others who find it hard to get out of bed.</h5>
+            
       </main>
       
    
