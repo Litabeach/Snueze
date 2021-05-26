e5298ebba5378d541086994e0e9b228d145ae175
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-25 21:52:07 -0500

    comment fix

--- client/src/components/Alert/index.js
+++ client/src/components/Alert/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {Alert} from 'react-bootstrap';
+// import React from 'react';
+// import {Alert} from 'react-bootstrap';
 
-function alertSuccess() {
-  const [show, setShow] = React.useState(flase)
-  return (
-      <Alert show={show} variant="success">
-      Sleep recorded successfully!
-    </Alert>
-  )
-}
+// function alertSuccess() {
+//   const [show, setShow] = React.useState(flase)
+//   return (
+//       <Alert show={show} variant="success">
+//       Sleep recorded successfully!
+//     </Alert>
+//   )
+// }
 
-export default alertSuccess;
+// export default alertSuccess;
