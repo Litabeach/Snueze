1405cf05bb3c977a7d80b4a5e6b43e4e0fd39496
Lita Beach <litabeach123@gmail.com>
2021-05-27 12:26:41 -0500

    little extra margin under subheading on community page

--- client/src/pages/Community/Community.js
+++ client/src/pages/Community/Community.js
@@ -1,9 +1,8 @@
 
 import React, { Component } from "react";
 import ChatRoom from '../../components/ChatRoom';
-import ChatDescription from '../../components/ChatDescriptions/ChatDescriptions';
 import chatDesc from '../../components/ChatDescriptions/chat-description.json';
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./style.css"
 
 
@@ -16,7 +15,7 @@ class Community extends Component {
     return (
       <Container fluid>
         <h1>Community Chat Rooms</h1>
-        <h5 className="subheading">Sometimes all we're really looking for is a person to connect with over our problems or someone to go to for advice. Utilize our collection of video chat rooms to talk to real people who are going through the same thing as you.</h5>
+        <h5 className="subheading" id="community-subheading">Sometimes all we're really looking for is a person to connect with over our problems or someone to go to for advice. Utilize our collection of video chat rooms to talk to real people who are going through the same thing as you.</h5>
             <ChatRoom />
       </Container>
 
